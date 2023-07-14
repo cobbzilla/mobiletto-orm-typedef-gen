eb4ecd56529151133d985ca4623082b254d848b2
Jonathan Cobb <jonathan@kyuss.org>
2023-07-14 09:55:53 -0400

    revert export style

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,18 +1,4 @@
-import { MobilettoOrmError, MobilettoOrmNotFoundError, MobilettoOrmTypeDef, MobilettoOrmSyncError, MobilettoOrmValidationError } from "mobiletto-orm-typedef";
-export { STANDARD_AUTOGEN_FILE_DISCLAIMER } from "./generate.js";
-export { generateTypeScriptType } from "./generators/tsType.js";
-export { generateYup } from "./generators/yup.js";
 export * from "mobiletto-orm-typedef";
-declare const _default: {
-    MobilettoOrmError: typeof MobilettoOrmError;
-    MobilettoOrmNotFoundError: typeof MobilettoOrmNotFoundError;
-    MobilettoOrmTypeDef: typeof MobilettoOrmTypeDef;
-    MobilettoOrmSyncError: typeof MobilettoOrmSyncError;
-    MobilettoOrmValidationError: typeof MobilettoOrmValidationError;
-    addError: (errors: import("mobiletto-orm-typedef").ValidationErrors, fieldPath: string, err: string) => void;
-    fsSafeName: (name: string) => string;
-    rand: (len?: number | undefined) => string;
-    sha: (val: string | number | boolean) => string;
-    MIN_ID_LENGTH: number;
-};
-export default _default;
+export * from "./generate.js";
+export * from "./generators/tsType.js";
+export * from "./generators/yup.js";
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,4 @@
-import {
-    MobilettoOrmError,
-    MobilettoOrmNotFoundError,
-    MobilettoOrmTypeDef,
-    MobilettoOrmSyncError,
-    MobilettoOrmValidationError,
-    addError,
-    fsSafeName,
-    rand,
-    sha,
-    MIN_ID_LENGTH,
-} from "mobiletto-orm-typedef";
-
-export { STANDARD_AUTOGEN_FILE_DISCLAIMER } from "./generate.js";
-export { generateTypeScriptType } from "./generators/tsType.js";
-export { generateYup } from "./generators/yup.js";
-
 export * from "mobiletto-orm-typedef";
-
-export default {
-    MobilettoOrmError,
-    MobilettoOrmNotFoundError,
-    MobilettoOrmTypeDef,
-    MobilettoOrmSyncError,
-    MobilettoOrmValidationError,
-    addError,
-    fsSafeName,
-    rand,
-    sha,
-    MIN_ID_LENGTH,
-};
+export * from "./generate.js";
+export * from "./generators/tsType.js";
+export * from "./generators/yup.js";
